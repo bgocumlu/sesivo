aaa973cd041e67811b9433523a0518148e8eb136
bgocumlu <bgocumlu@users.noreply.github.com>
2026-07-06 23:05:22 +0300

    Package only the macOS app bundle

--- tools/package-macos.sh
+++ tools/package-macos.sh
@@ -7,4 +7,5 @@ package_dir="${PACKAGE_DIR:-${build_dir}/package}"
 
 cmake -S . -B "$build_dir" -DCMAKE_BUILD_TYPE="$config" -DJAM_BUILD_CLIENT=ON
 cmake --build "$build_dir" --target client --config "$config"
+rm -rf "$package_dir"
 cpack --config "$build_dir/CPackConfig.cmake" -G DragNDrop -C "$config" -B "$package_dir"

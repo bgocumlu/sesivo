fcc2e29bb67406da83d26e6387d435e856d17cd5
bgocumlu <bgocumlu@users.noreply.github.com>
2026-07-06 23:12:34 +0300

    Set compact macOS DMG layout

--- cmake/packaging.cmake
+++ cmake/packaging.cmake
@@ -9,7 +9,7 @@ if(APPLE AND JAM_BUILD_CLIENT)
     set(CPACK_PACKAGE_NAME "sesivo")
     set(CPACK_PACKAGE_VENDOR "sesivo")
     set(CPACK_PACKAGE_VERSION "0.1.0")
-    set(CPACK_PACKAGE_FILE_NAME "sesivo-${CPACK_PACKAGE_VERSION}-macos")
+    set(CPACK_PACKAGE_FILE_NAME "sesivo-${CPACK_PACKAGE_VERSION}")
     set(CPACK_COMPONENTS_ALL client)
     set(CPACK_INSTALL_CMAKE_PROJECTS
         "${CMAKE_BINARY_DIR};${CMAKE_PROJECT_NAME};client;/"
@@ -17,6 +17,9 @@ if(APPLE AND JAM_BUILD_CLIENT)
     set(CPACK_GENERATOR "DragNDrop")
     set(CPACK_DMG_VOLUME_NAME "sesivo")
     set(CPACK_DMG_FORMAT "UDZO")
+    set(CPACK_DMG_DS_STORE_SETUP_SCRIPT
+        "${CMAKE_CURRENT_SOURCE_DIR}/packaging/macos/dmg_layout.applescript"
+    )
 
     include(CPack)
 endif()
